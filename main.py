26401628c167b73480546632858115183cdb60ff
carstenkueperkoch <c.kueperkoch@hotmail.com>
2025-10-13 20:59:22 +0200

    handling fps

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from constants import * #wildcard import because its a small project
 
 def main():
     pygame.init()
-
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -13,6 +14,7 @@ def main():
             
         screen.fill(color=(0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
