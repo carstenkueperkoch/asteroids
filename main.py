3387ceb252fac5d27e3ce0a8d44bf9240f5bae72
carstenkueperkoch <c.kueperkoch@hotmail.com>
2025-10-13 21:42:39 +0200

    added player

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import * #wildcard import because its a small project
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,7 @@ def main():
                 return
             
         screen.fill(color=(0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
